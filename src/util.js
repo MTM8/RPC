738cb6c3a9a6ba692fc70b7fd3c36aab56718a52
MTM8 <67551800+MTM8@users.noreply.github.com>
2020-11-11 16:03:09 -0600

    Removed electron

--- src/util.js
+++ src/util.js
@@ -1,14 +1,6 @@
 'use strict';
 
 let register;
-try {
-  const { app } = require('electron');
-  register = app.setAsDefaultProtocolClient.bind(app);
-} catch (err) {
-  try {
-    register = require('register-scheme');
-  } catch (e) {} // eslint-disable-line no-empty
-}
 
 if (typeof register !== 'function') {
   register = () => false;
